5a7020f0c09a152fd40c89801f2bf69c0be5e85c
Chad Carter <kewlniss@gmail.com>
2017-09-29 10:52:08 -0400

    Updated example scene to show how to play a note

--- Assets/Scripts/MIDIPlayer.cs
+++ Assets/Scripts/MIDIPlayer.cs
@@ -12,13 +12,15 @@ public class MIDIPlayer : MonoBehaviour
     //Public
     //Check the Midi's file folder for different songs
     public string midiFilePath = "Midis/Groove.mid";
-    
+    public bool ShouldPlayFile = true;
+
     //Try also: "FM Bank/fm" or "Analog Bank/analog" for some different sounds
     public string bankFilePath = "GM Bank/gm";
     public int bufferSize = 1024;
     public int midiNote = 60;
     public int midiNoteVolume = 100;
-    public int midiInstrument = 1;
+    [Range(0, 127)] //From Piano to Gunshot
+    public int midiInstrument = 0;
     //Private 
     private float[] sampleBuffer;
     private float gain = 1f;
@@ -34,10 +36,10 @@ public class MIDIPlayer : MonoBehaviour
     {
         midiStreamSynthesizer = new StreamSynthesizer(44100, 2, bufferSize, 40);
         sampleBuffer = new float[midiStreamSynthesizer.BufferSize];
-
+        
         midiStreamSynthesizer.LoadBank(bankFilePath);
 
-        LoadSong(midiFilePath);
+        midiSequencer = new MidiSequencer(midiStreamSynthesizer);
 
         //These will be fired by the midiSequencer when a song plays. Check the console for messages if you uncomment these
         //midiSequencer.NoteOnEvent += new MidiSequencer.NoteOnEventHandler (MidiNoteOnHandler);
@@ -46,7 +48,6 @@ public class MIDIPlayer : MonoBehaviour
 
     void LoadSong(string midiPath)
     {
-        midiSequencer = new MidiSequencer(midiStreamSynthesizer);
         midiSequencer.LoadMidi(midiPath, false);
         midiSequencer.Play();
     }
@@ -62,25 +63,46 @@ public class MIDIPlayer : MonoBehaviour
     void Update()
     {
         if (!midiSequencer.isPlaying)
+        {
             //if (!GetComponent<AudioSource>().isPlaying)
-            LoadSong(midiFilePath);
-    }
+            if (ShouldPlayFile)
+            {
+                LoadSong(midiFilePath);
+            }
+        }
+        else if (!ShouldPlayFile)
+        {
+            midiSequencer.Stop(true);
+        }
 
-    // See http://unity3d.com/support/documentation/ScriptReference/MonoBehaviour.OnAudioFilterRead.html for reference code
-    //	If OnAudioFilterRead is implemented, Unity will insert a custom filter into the audio DSP chain.
-    //
-    //	The filter is inserted in the same order as the MonoBehaviour script is shown in the inspector. 	
-    //	OnAudioFilterRead is called everytime a chunk of audio is routed thru the filter (this happens frequently, every ~20ms depending on the samplerate and platform). 
-    //	The audio data is an array of floats ranging from [-1.0f;1.0f] and contains audio from the previous filter in the chain or the AudioClip on the AudioSource. 
-    //	If this is the first filter in the chain and a clip isn't attached to the audio source this filter will be 'played'. 
-    //	That way you can use the filter as the audio clip, procedurally generating audio.
-    //
-    //	If OnAudioFilterRead is implemented a VU meter will show up in the inspector showing the outgoing samples level. 
-    //	The process time of the filter is also measured and the spent milliseconds will show up next to the VU Meter 
-    //	(it turns red if the filter is taking up too much time, so the mixer will starv audio data). 
-    //	Also note, that OnAudioFilterRead is called on a different thread from the main thread (namely the audio thread) 
-    //	so calling into many Unity functions from this function is not allowed ( a warning will show up ). 	
-    private void OnAudioFilterRead(float[] data, int channels)
+        if (Input.GetButtonDown("Fire1"))
+        {
+            midiStreamSynthesizer.NoteOn(0, midiNote, midiNoteVolume, midiInstrument);
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            midiStreamSynthesizer.NoteOff(0, midiNote);
+        }
+
+
+        }
+
+        // See http://unity3d.com/support/documentation/ScriptReference/MonoBehaviour.OnAudioFilterRead.html for reference code
+        //	If OnAudioFilterRead is implemented, Unity will insert a custom filter into the audio DSP chain.
+        //
+        //	The filter is inserted in the same order as the MonoBehaviour script is shown in the inspector. 	
+        //	OnAudioFilterRead is called everytime a chunk of audio is routed thru the filter (this happens frequently, every ~20ms depending on the samplerate and platform). 
+        //	The audio data is an array of floats ranging from [-1.0f;1.0f] and contains audio from the previous filter in the chain or the AudioClip on the AudioSource. 
+        //	If this is the first filter in the chain and a clip isn't attached to the audio source this filter will be 'played'. 
+        //	That way you can use the filter as the audio clip, procedurally generating audio.
+        //
+        //	If OnAudioFilterRead is implemented a VU meter will show up in the inspector showing the outgoing samples level. 
+        //	The process time of the filter is also measured and the spent milliseconds will show up next to the VU Meter 
+        //	(it turns red if the filter is taking up too much time, so the mixer will starv audio data). 
+        //	Also note, that OnAudioFilterRead is called on a different thread from the main thread (namely the audio thread) 
+        //	so calling into many Unity functions from this function is not allowed ( a warning will show up ). 	
+        private void OnAudioFilterRead(float[] data, int channels)
     {
         //This uses the Unity specific float method we added to get the buffer
         midiStreamSynthesizer.GetNext(sampleBuffer);
